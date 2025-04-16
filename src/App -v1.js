e91bc3e4593ed11b968fa14113fc6cf4c4217881
Sajad Atharyan <sajadath1@gmail.com>
2025-04-16 22:34:09 +0330

    https request fix

--- src/App -v1.js
+++ src/App -v1.js
@@ -259,7 +259,7 @@ function MovieDetail({ watched, onAddWatched, onCloseMovie, selectedId }) {
     async function getMovieDetails() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
